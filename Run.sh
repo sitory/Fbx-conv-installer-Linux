b58eb42bf2a243f22919c87e74d9f06258ac8e58
Yzubi <42348748+Yzubi@users.noreply.github.com>
2018-12-01 02:30:14 -0200

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,6 +1,8 @@
 cd "${0%/*}"
+options="$1"
+
 export LD_LIBRARY_PATH=.
-find models -maxdepth 1 -type f -exec ./fbx-conv -f -p {} \;
+find models -maxdepth 1 -type f -exec ./fbx-conv -f "$options" {} \;
 
 mv -v models/* done/
 mv done/*.g3db converted
