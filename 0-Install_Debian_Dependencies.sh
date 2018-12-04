63071a448301889a2d65b6ecd89fedb5b81accb7
Yzubi <42348748+Yzubi@users.noreply.github.com>
2018-12-04 15:58:38 -0200

    Update 0-Install_Debian_Dependencies.sh

--- 0-Install_Debian_Dependencies.sh
+++ 0-Install_Debian_Dependencies.sh
@@ -1,7 +1,1 @@
-#DEP
-sudo apt-get install libpng-dev
-sudo apt-get install zlib1g-dev
-#DEP2
-sudo apt-get install sed
-sudo apt-get install git
-sudo apt-get install wget
+sudo apt-get install libpng-dev zlib1g-dev sed git wget make gcc
