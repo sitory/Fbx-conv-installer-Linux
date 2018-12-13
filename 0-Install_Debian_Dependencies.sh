97a7cdc6ce9d2f3c20b4e4cc766a69164e3f2fdd
Yzubi <42348748+Yzubi@users.noreply.github.com>
2018-12-13 17:07:02 -0200

    Update 0-Install_Debian_Dependencies.sh

--- 0-Install_Debian_Dependencies.sh
+++ 0-Install_Debian_Dependencies.sh
@@ -1,1 +1,8 @@
+if [ "$(uname -m | grep '64')" != "" ]; then
+echo x64 operating system.
+sudo dpkg --add-architecture i386
+sudo apt-get install libpng-dev zlib1g-dev sed git wget make gcc lib32stdc++6 lib32gcc1 lib32z1
+else
+echo x86 operating system.
 sudo apt-get install libpng-dev zlib1g-dev sed git wget make gcc
+fi
