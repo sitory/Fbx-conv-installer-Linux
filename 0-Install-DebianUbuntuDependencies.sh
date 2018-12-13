ee53cdb72a07c07ffa772beae721c064d3625deb
Yzubi <42348748+Yzubi@users.noreply.github.com>
2018-12-13 17:56:51 -0200

    Update 0-Install-DebianUbuntuDependencies.sh

--- 0-Install-DebianUbuntuDependencies.sh
+++ 0-Install-DebianUbuntuDependencies.sh
@@ -1,6 +1,7 @@
 if [ "$(uname -m | grep '64')" != "" ]; then
 echo x64 operating system.
-sudo dpkg --add-architecture i386
+#sudo dpkg --add-architecture i386
+#sudo apt-get update
 sudo apt-get install libpng-dev zlib1g-dev sed git wget make gcc lib32stdc++6 lib32gcc1 lib32z1
 else
 echo x86 operating system.
